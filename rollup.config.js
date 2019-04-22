a9b7bb0fa6892b8654dfba90011bcd7c0423375d
Maciek Sakrejda <m.sakrejda@gmail.com>
2019-04-21 22:42:06 -0700

    Spaces

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,18 @@ import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default [
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	{
-		input: 'src/main.js',
-		external: [ 'react' ],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' },
-		],
-		plugins: [
-			babel({
-				exclude: [ 'node_modules/**' ]
-			})
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/main.js',
+    external: [ 'react' ],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      babel({
+        exclude: [ 'node_modules/**' ]
+      })
+    ]
+  }
 ]
